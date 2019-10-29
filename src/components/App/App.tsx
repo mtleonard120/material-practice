49c58b3f72384a506c1f973c442b65826277acad
Michael Leonard <michael.leonard@cqlcorp.com>
2019-10-29 11:07:04 -0400

    Create basic theme

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import styles from './App.module.scss';
+import React from 'react'
 
+// packages
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
+import {orange, red} from '@material-ui/core/colors'
+import {Checkbox} from '@material-ui/core'
+
+// theme
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: red[500],
+        },
+        error: {
+            main: orange[500],
+        },
+    },
+})
+
+// primary component
 export const App: React.FC = () => {
-  return (
-    <div className={styles.app}>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Checkbox defaultChecked />
+        </ThemeProvider>
+    )
 }
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// styles
+import './index.css'
+
+// components
+import {App} from './components'
+
+ReactDOM.render(<App />, document.getElementById('root'))
